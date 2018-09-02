672132a904231d5e83b01826fcc2190117bb3e5c
avin's desktop <avinchen111414@gmail.com>
2018-09-02 20:53:09 +0800

    add: FresnelReflector

---
+++ src/brdfs/fresnelreflector.cpp
@@ -0,0 +1,67 @@
+#include <cmath>
+
+#include "utilities/shaderec.h"
+#include "utilities/vector3d.h"
+#include "fresnelreflector.h"
+
+FresnelReflector::FresnelReflector()
+	: BRDF(), cr(1.0f)
+{}
+
+FresnelReflector::FresnelReflector(const FresnelReflector& fr)
+	: BRDF(fr), cr(fr.cr)
+{}
+
+FresnelReflector& FresnelReflector::operator=(const FresnelReflector& rhs)
+{
+	if (this == &rhs)
+		return *this;
+
+	BRDF::operator=(rhs);
+	cr = rhs.cr;
+	return *this;
+}
+
+FresnelReflector* FresnelReflector::clone() const
+{
+	return new FresnelReflector(*this);
+}
+
+RGBColor FresnelReflector::sample_f(const ShadeRec& sr, const Vector3D& wo, Vector3D& wi) const
+{
+	float ndotwo = sr.normal * wo;
+	wi = -wo + 2 * ndotwo * sr.normal;
+	return this->fresnel(sr) * cr / (sr.normal * wi);
+}
+
+void FresnelReflector::set_eta(float eta_in, float eta_out)
+{
+	this->eta_in = eta_in; this->eta_out = eta_out;
+}
+
+float FresnelReflector::fresnel(const ShadeRec& sr) const
+{
+	Normal normal(sr.normal);
+	float ndotd = normal * -sr.ray.d;
+
+	float eta = 0.0f;
+	// Ray hits inside surface
+	if (ndotd < 0.0f)
+	{
+		normal = -normal;
+		eta = eta_out / eta_in;
+	}
+	else
+		eta = eta_in / eta_out;
+
+	float cos_theta_i = normal * -sr.ray.d;
+	float cos_theta_t = sqrt(1.0f - (1.0f - cos_theta_i * cos_theta_i) / (eta * eta));
+
+	float r_parallel = (eta * cos_theta_i - cos_theta_t) / 
+		(eta * cos_theta_i + cos_theta_t);
+	float r_perpendicular = (cos_theta_i - eta * cos_theta_t) /
+		(cos_theta_i + eta * cos_theta_t);
+	float kr = 0.5 * (r_parallel * r_parallel +
+		r_perpendicular * r_perpendicular);
+	return kr;
+}
---
+++ src/brdfs/fresnelreflector.h
@@ -0,0 +1,35 @@
+#ifndef __FRESNELREFLECTOR_H__
+#define __FRESNELREFLECTOR_H__
+
+#include "BRDF.h"
+
+class ShadeRec;
+class Vector3D;
+class FresnelReflector: public BRDF
+{
+public:
+	FresnelReflector();
+
+	FresnelReflector(const FresnelReflector& fr);
+
+	FresnelReflector& operator= (const FresnelReflector& rhs);
+
+	virtual ~FresnelReflector();
+
+	virtual FresnelReflector* clone() const;
+
+	virtual RGBColor FresnelReflector::sample_f(const ShadeRec& sr, const Vector3D& wo, Vector3D& wi) const;
+
+	float fresnel(const ShadeRec& sr) const;
+
+	inline void set_eta(float eta_in, float eta_out);
+protected:
+
+private:
+	RGBColor cr;
+	float eta_in;
+	float eta_out;
+
+};
+
+#endif
